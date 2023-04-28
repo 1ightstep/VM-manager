7cf1be8b89afa9db466246657abf4d12d9e4b7e6
kAvaras <93688603+kAvaras@users.noreply.github.com>
2023-04-28 13:37:24 -0700

    client.py

--- Server-n-client/connect.py
+++ Server-n-client/connect.py
@@ -14,7 +14,7 @@ class client:
             self.server_password = sys.argv[3]
             self.client_root = sys.argv[4]
             self.encoding = "latin-1"
-            if "-h" in sys.argv or "--help" sys.argv:
+            if "-h" in sys.argv or "--help" in sys.argv:
                 print("python3 client.py <server ip> <server port> <server password> <root password>")
                 quit()
             try:
@@ -29,44 +29,47 @@ class client:
             quit()
     def manage(self):
         while True:
-            command = self.s.recv(1024*100).decode(self.encoding)
-            split_command = command.split("__SPACE__")
-            if split_command[0] == "fetch":
-                if os.path.exists(split_command[1]):
-                    self.s.send("__FILE_EXISTS__".encode(self.encoding))
-                    time.sleep(0.1)
-                    with open(split_command[1], "rb") as f:
-                        file_byte = f.read()
-                        chunks = list(funcy.chunks(10, file_byte))
-                        for chunk in chunks:
-                            self.s.send(chunk)
-                        self.s.send(b"__EOFL__")
+            try:
+                command = self.s.recv(1024*100).decode(self.encoding)
+                split_command = command.split("__SPACE__")
+                if split_command[0] == "fetch":
+                    if os.path.exists(split_command[1]):
+                        self.s.send("__FILE_EXISTS__".encode(self.encoding))
+                        time.sleep(0.1)
+                        with open(split_command[1], "rb") as f:
+                            file_byte = f.read()
+                            chunks = list(funcy.chunks(10, file_byte))
+                            for chunk in chunks:
+                                self.s.send(chunk)
+                            self.s.send(b"__EOFL__")
+                            f.close()
+                    else:
+                        self.s.send(b"__NO_FILE__")
+                if split_command[0] == "send":
+                    path = f'{split_command[1].replace(".", "")}_{"".join(random.choice(string.ascii_lowercase) for i in range(5))}'
+                    os.mkdir(path)
+                    with open(f"{path}/{split_command[1]}", "wb") as f:
+                        recv_data = b'<BYTE>'
+                        byte_array = []
+                        while True:
+                            recv_data = self.s.recv(1024)
+                            if b'__EOFL__' in recv_data:
+                                byte_array.append(recv_data.replace(b'__EOFL__', b''))
+                                break
+                            else:
+                                byte_array.append(recv_data)
+                                print(recv_data, end="")
+                        f.write(b''.join(byte_array))
                         f.close()
-                else:
-                    self.s.send(b"__NO_FILE__")
-            if split_command[0] == "send":
-                path = f'{split_command[1].replace(".", "")}_{"".join(random.choice(string.ascii_lowercase) for i in range(5))}'
-                os.mkdir(path)
-                with open(f"{path}/{split_command[1]}", "wb") as f:
-                    recv_data = b'<BYTE>'
-                    byte_array = []
-                    while True:
-                        recv_data = self.s.recv(1024)
-                        if b'__EOFL__' in recv_data:
-                            byte_array.append(recv_data.replace(b'__EOFL__', b''))
-                            break
-                        else:
-                            byte_array.append(recv_data)
-                            print(recv_data, end="")
-                    f.write(b''.join(byte_array))
-                    f.close()
-            if split_command[0] == "execute":
-                output = subprocess.getoutput(split_command[1])
-                if output != "":
-                    for char in output:
-                        self.s.send(char.encode(self.encoding))
-                    self.s.send("__EOFL__".encode(self.encoding))
-                else:
-                    self.s.send("Empty Output".encode(self.encoding))
+                if split_command[0] == "execute":
+                    output = subprocess.getoutput(split_command[1])
+                    if output != "":
+                        for char in output:
+                            self.s.send(char.encode(self.encoding))
+                        self.s.send("__EOFL__".encode(self.encoding))
+                    else:
+                        self.s.send("Empty Output".encode(self.encoding))
+            except:
+                pass
 client = client()
 client.manage()
